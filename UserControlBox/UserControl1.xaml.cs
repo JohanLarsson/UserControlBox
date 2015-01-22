2887b3d374785611a67fbbef73f57890017de4e2
Johan Larsson <johan_gavle1@hotmail.com>
2015-01-22 23:35:01 +0100

    Initial

---
+++ UserControlBox/UserControl1.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace UserControlBox
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public static readonly DependencyProperty UriProperty = DependencyProperty.Register(
+            "Uri", typeof (Uri), typeof (UserControl1), new PropertyMetadata(default(Uri)));
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public Uri Uri
+        {
+            get { return (Uri)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+    }
+}
